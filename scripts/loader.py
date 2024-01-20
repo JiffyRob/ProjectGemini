ffc43fd59e5bb52c54bab60487baba1ed91b0526
John Robinson <jfrobinson767@gmail.com>
2024-01-19 22:08:19 -0600

    added file caching

--- scripts/loader.py
+++ scripts/loader.py
@@ -3,29 +3,33 @@ import pathlib
 
 import pygame
 import pygame._sdl2 as sdl2
+import functools
 
 
 class Loader:
     def __init__(self, renderer):
         self.renderer = renderer
-        self.cache = {}
         self.base_path = pathlib.Path("assets/")
 
+    @functools.cache
     def get_text(self, path):
-        if path not in self.cache:
-            with open("path", "r") as file:
-                self.cache[path] = file.read()
+        with pathlib.Path(self.base_path, path).open() as file:
+            return file.read()
 
+    @functools.cache
+    def get_json(self, path):
+        return json.load((self.base_path / path).open())
+
+    @functools.cache
+    def get_surface(self, path):
+        return pygame.image.load(pathlib.Path(self.base_path, path))
+
+    @functools.cache
     def get_texture(self, path, name):
-        # TODO: implement file cache
         path = pathlib.Path(path)
         key = f"{path}$+${name}"  # if a file path follows THIS format it's the developer's problem
-        if key not in self.cache:
-            texture = sdl2.Texture.from_surface(
-                self.renderer,
-                pygame.image.load(self.base_path / path.with_suffix(".png")),
-            )
-            with (self.base_path / path.with_suffix(".json")).open() as file:
-                data = json.loads(file.read())
-            self.cache[key] = sdl2.Image(texture, data[name])
-        return self.cache[key]
+        texture = sdl2.Texture.from_surface(
+            self.renderer,
+            self.get_surface(path.with_suffix(".png")),
+        )
+        return sdl2.Image(texture, self.get_json(path.with_suffix(".json"))[name])
