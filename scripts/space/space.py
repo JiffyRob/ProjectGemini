fc9f350dcde6d2491db8e855b6e91ec20a348392
JiffyRob <jfrobinson767@gmail.com>
2024-12-24 20:56:47 -0600

    GPU accelerated Space

--- scripts/space/space.py
+++ scripts/space/space.py
@@ -1,5 +1,8 @@
+import struct
+
 import numpy
 import pygame
+import zengl
 
 from scripts import game_state, util_draw
 from scripts.animation import AnimatedSurface
@@ -7,8 +10,16 @@ from scripts.space import gui3d, math3d, sprite3d
 
 
 class Space(game_state.GameState):
+    CIRCLE_RESOLUTION = 16
+    SPRITE_COUNT = 2
+
+    PLANET_STAR = 0
+    PLANET_TERRA1 = 1
+    PLANET_TERRA2 = 2
+    PLANET_KEERGAN = 3
+
     def __init__(self, game):
-        super().__init__(game, color="black")
+        super().__init__(game, color="black", opengl=True)
         # self.game.renderer.logical_size = (1920, 1080)
         # in world space y is vertical, and x and z are horizontal
         # in game terms, y is Quarth-Mist, x is East-West, and z is North-South
@@ -18,7 +29,7 @@ class Space(game_state.GameState):
             math3d.Quaternion(),
             pygame.Vector2(util_draw.RESOLUTION) / 2,
             pygame.Vector2(60, 60),  # TODO : FOV
-            400,
+            200,
             2000,
         )
         ship_rect = pygame.Rect(0, 0, 48, 32)
@@ -32,51 +43,81 @@ class Space(game_state.GameState):
         self.ship_overlay = self.game.loader.get_surface_scaled_to(
             "ship-inside.png", util_draw.RESOLUTION
         )
-        self.static_sprites = sprite3d.StaticSpriteGroup(self, 10000, 6)
-        sizes = ((16, 16), (9, 9), (5, 5), (2, 2))
-        blue_textures = {
-            size: self.game.loader.get_image("stars", f"blue{i + 1}")
-            for i, size in enumerate(sizes)
-        }
-        blue_textures[(0, 0)] = pygame.Surface((0, 0)).convert()
-        self.static_sprites.add_textures(
-            "blue",
-            blue_textures,
-        )
-        yellow_textures = {
-            size: self.game.loader.get_image("stars", f"yellow{i + 1}")
-            for i, size in enumerate(sizes)
-        }
-        yellow_textures[(0, 0)] = pygame.Surface((0, 0)).convert()
-        self.static_sprites.add_textures(
-            "yellow",
-            yellow_textures,
-        )
-        self.planet_ids = {}
-        planets = (("GeminiII", (0, 0, 1000)), ("Keergan", (0, 0, -1000)))
-        for name, position in planets:
-            planet_textures = {
-                (size, size): AnimatedSurface(
-                    self.game.loader.get_spritesheet(
-                        f"planets/{name}{size}", (size, size)
-                    )
-                )
-                for size in (6, 16, 32, 64, 128, 192)
-            }
-            self.static_sprites.add_textures(
-                name,
-                planet_textures,
-            )
-            self.planet_ids[name] = self.static_sprites.add_sprite(
-                position, name, (128, 128)
-            )
+        self.gui_surface = pygame.Surface((util_draw.RESOLUTION), pygame.SRCALPHA)
+        self.gui_gl_surface = self.game.context.image(util_draw.RESOLUTION)
 
-        for pos in numpy.random.uniform(
-            low=-2000, high=2000, size=(10000 - len(planets), 3)
-        ):
-            self.static_sprites.add_sprite(
-                tuple(pos), numpy.random.choice(["blue", "yellow"])
-            )
+        rng = numpy.random.default_rng(1)  # TODO: random seeding?
+        self.locations = rng.uniform(low=-2000, high=2000, size=(self.SPRITE_COUNT, 3)).astype("f4")
+        self.planets = numpy.zeros(self.SPRITE_COUNT, "i4")
+        self.radii = numpy.zeros(self.SPRITE_COUNT, "f4") + 0.3
+
+        self.planet_ids = {}
+        planets = ((self.PLANET_TERRA1, (0, 0, -1000)), (self.PLANET_KEERGAN, (0, 0, 1000)))
+        for i, (planet, location) in enumerate(planets):
+            self.locations[i] = location
+            self.planets[i] = planet
+            self.radii[i] = 1
+
+        print(self.locations)
+        print(self.planets)
+        print(self.radii)
+
+        angle = numpy.linspace(0.0, numpy.pi * 2.0, self.CIRCLE_RESOLUTION)
+        xy = numpy.array([numpy.cos(angle), numpy.sin(angle)])
+        vertex_buffer = self.game.context.buffer(xy.T.astype('f4').tobytes())
+
+        self.depth_buffer = self.game.context.image(util_draw.RESOLUTION, "depth24plus")
+        self.pipeline = self.game.context.pipeline(
+            vertex_shader=self.game.loader.get_vertex_shader("space"),
+            fragment_shader=self.game.loader.get_fragment_shader("planet"),
+            framebuffer=[self.game.gl_window_surface, self.depth_buffer],
+            topology="triangle_fan",
+            uniforms={
+                "time": 0.0,
+                "near_z": 400.0,
+                "far_z": 2000.0,
+                "viewpos_x": 10.0,
+                "viewpos_y": 0.0,
+                "viewpos_z": 0.0,
+                "rot_x": 0.0,
+                "rot_y": 0.0,
+                "rot_z": 0.0,
+                "rot_theta": 0.0,
+            },
+            vertex_buffers=[
+                *zengl.bind(self.game.context.buffer(self.locations), "3f /i", 0),
+                *zengl.bind(self.game.context.buffer(self.planets), "1i /i", 1),
+                *zengl.bind(vertex_buffer, "2f", 2),
+                *zengl.bind(self.game.context.buffer(self.radii), "1f /i", 3),
+            ],
+            vertex_count=self.CIRCLE_RESOLUTION,
+            instance_count=self.SPRITE_COUNT,
+        )
+        self.gui_pipeline = self.game.context.pipeline(
+            vertex_shader=self.game.loader.get_vertex_shader("scale"),
+            fragment_shader=self.game.loader.get_fragment_shader("overlay"),
+            framebuffer=[self.game.gl_window_surface],
+            topology="triangle_strip",
+            vertex_count=4,
+            layout=[
+                {
+                    "name": "input_texture",
+                    "binding": 0,
+                }
+            ],
+            resources=[
+                {
+                    "type": "sampler",
+                    "binding": 0,
+                    "image": self.gui_gl_surface,
+                    "min_filter": "nearest",
+                    "mag_filter": "nearest",
+                    "wrap_x": "clamp_to_edge",
+                    "wrap_y": "clamp_to_edge",
+                }
+            ]
+        )
+
         self.turn_speeds = {
             "up": 0,
             "down": 0,
@@ -84,13 +125,16 @@ class Space(game_state.GameState):
             "right": 0,
         }
         self.turn_delta = 0.007
-        self.max_turn_speed = 0.4
+        self.max_turn_speed = 0.8
         self.forward_delta = 10
-        self.min_forward_speed = 150
+        self.min_forward_speed = 40
         self.max_forward_speed = 1000
         self.forward_speed = self.min_forward_speed
+        self.age = 0
+        # self.camera.rotation *= math3d.Quaternion(3.1415, (0, 1, 0))
 
     def update(self, dt):
+        self.age += dt
         pressed = self.game.input_queue.just_pressed
         if "quit" in pressed:
             self.game.quit()
@@ -159,49 +203,28 @@ class Space(game_state.GameState):
             self.forward_speed, self.min_forward_speed, self.max_forward_speed
         )
         motion = pygame.Vector3(0, 0, self.forward_speed * dt)
-        self.static_sprites.update(dt)
         self.camera.pos += self.camera.rotation * motion
+
+        self.pipeline.uniforms["time"][:] = struct.pack("f", self.age)
+        self.pipeline.uniforms["viewpos_x"][:] = struct.pack("f", self.camera.pos.x)
+        self.pipeline.uniforms["viewpos_y"][:] = struct.pack("f", self.camera.pos.y)
+        self.pipeline.uniforms["viewpos_z"][:] = struct.pack("f", self.camera.pos.z)
+        self.pipeline.uniforms["rot_x"][:] = struct.pack("f", self.camera.rotation.vector.x)
+        self.pipeline.uniforms["rot_y"][:] = struct.pack("f", self.camera.rotation.vector.y)
+        self.pipeline.uniforms["rot_z"][:] = struct.pack("f", self.camera.rotation.vector.z)
+        self.pipeline.uniforms["rot_theta"][:] = struct.pack("f", self.camera.rotation.real)
+
+        print(self.camera.pos)
+
         for sprite in self.gui:
             sprite.update(dt)
         return True
 
     def draw(self):
-        self.static_sprites.dirty_draw(self.camera)
-        projection_matrix = numpy.array(
-            (
-                (self.camera.near_z, 0, 0, 0),
-                (0, self.camera.near_z, 0, 0),
-                (
-                    0,
-                    0,
-                    self.camera.far_z + self.camera.near_z,
-                    -self.camera.far_z * self.camera.near_z,
-                ),
-                (0, 0, 1, 0),
-            ),
-            numpy.float64,
-        )
-        negated_rotation = -self.camera.rotation
-        for sprite in self.sprites:
-            # TODO: Proper FOV??
-            # translate and rotate (relative position * relative rotation)
-            relative_pos = negated_rotation * (sprite.pos - self.camera.pos)
-            # scale (cheating)
-            scale_factor = self.camera.near_z / relative_pos.z
-            sprite.rect.width = sprite.width * scale_factor
-            sprite.rect.height = sprite.height * scale_factor
-            # project
-            screen_pos = projection_matrix @ numpy.array(
-                (relative_pos.x, relative_pos.y, relative_pos.z, 1)
-            )
-            screen_pos = (
-                pygame.Vector3(screen_pos[0], screen_pos[1], screen_pos[2])
-                / screen_pos[3]
-            )
-            # draw
-            if self.camera.near_z <= screen_pos[2] <= self.camera.far_z:
-                sprite.rect.center = screen_pos.xy + self.camera.center
-                self.game.renderer.blit(sprite.image, sprite.rect)
-        # TODO: GUI
+        self.gui_surface.fill((0, 0, 0, 0))
         for sprite in self.gui:
-            sprite.draw(self.game.window_surface)
+            sprite.draw(self.gui_surface)
+        self.depth_buffer.clear()
+        self.gui_gl_surface.write(pygame.image.tobytes(self.gui_surface, "RGBA", True))
+        self.pipeline.render()
+        self.gui_pipeline.render()
---
+++ shaders/space.vert
@@ -0,0 +1,48 @@
+#version 300 es
+
+precision lowp float;
+
+layout (location=0) in vec3 loc;
+layout (location=1) in int planet_id_;
+layout (location=2) in vec2 vertex;
+layout (location=3) in float radius;
+
+uniform float viewpos_x;
+uniform float viewpos_y;
+uniform float viewpos_z;
+
+uniform float rot_theta;
+uniform float rot_x;
+uniform float rot_y;
+uniform float rot_z;
+uniform float near_z;
+uniform float far_z;
+
+out vec2 instance_coord;
+flat out int planet_id;
+flat out int flippy;
+
+void main() {
+    // imagine using matrices...HAH!
+    vec3 moved = loc;
+    // translate point
+    moved -= vec3(viewpos_x, viewpos_y, viewpos_z);
+    // rotate point
+    vec3 rotvec = -vec3(rot_x, rot_y, rot_z);
+    vec3 cross_product = cross(rotvec, moved);
+    moved = moved + (cross_product * 2.0 * rot_theta) + (2.0 * cross(rotvec, cross_product));
+    // z scaling
+    float scale_factor = near_z / moved.z;
+    float scaled_radius = radius * scale_factor;
+    moved.xy *= scale_factor;
+    moved.z = (moved.z - near_z) * 2.0 / (far_z - near_z) - 1.0;
+    // X, Y scaling
+    // moved.x *= 1.0 / 256.0;
+    // moved.y *= 1.0 / 256.0;
+    // don't ask why I multiply and divide by 2 here
+    // (I don't know)
+    gl_Position = vec4(moved.xy + vertex * scaled_radius * 2.0, moved.z, 1.0);
+    instance_coord = vertex / 2.0;
+    planet_id = planet_id_;
+    flippy = gl_InstanceID % 2;
+}
